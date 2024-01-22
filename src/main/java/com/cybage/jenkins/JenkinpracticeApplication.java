4dd4690bb5e034e2e8f42bc25c439f032b0ff9f1
riddhi <riddhit@cybage.com>
2024-01-22 17:46:16 +0530

    first commit

---
+++ src/main/java/com/cybage/jenkins/JenkinpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.cybage.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybage/jenkins/JenkinpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybage.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
