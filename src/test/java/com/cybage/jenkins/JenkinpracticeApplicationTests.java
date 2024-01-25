6aa70a0c43f61e79a8f5c63f160478360e7187c4
riddhi <riddhit@cybage.com>
2024-01-25 11:38:08 +0530

    second commit

--- src/test/java/com/cybage/jenkins/JenkinpracticeApplicationTests.java
+++ src/test/java/com/cybage/jenkins/JenkinpracticeApplicationTests.java
@@ -1,13 +1,21 @@
 package com.cybage.jenkins;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinpracticeApplicationTests {
+	public static Logger logger=LoggerFactory.getLogger(JenkinpracticeApplication.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("Test executing");
+		logger.info("test seconf running");
+		assertEquals(true,true);
 	}
 
 }
